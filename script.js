f50187de87788bdeab5fe0abddcdc3b0283b07bd
Eduardo Barbosa <95145702+eduhbarb@users.noreply.github.com>
2023-01-13 20:09:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function abreModalCV(){
   $('.modal-cv').fadeToggle()
 }
+
+$(document).ready(function(){
+  $(".crow-float,.crow-menu-button").click(function(){
+      $(".crow-menu").animate({
+          width: "toggle"
+      });
+  });
+});
